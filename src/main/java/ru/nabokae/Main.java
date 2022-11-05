726e7d690b1c20fc9257e595315caf0d68f43311
NabokaEA <nabokae@yandex.ru>
2022-11-05 15:45:31 +0300

    2

--- src/main/java/ru/nabokae/Main.java
+++ src/main/java/ru/nabokae/Main.java
@@ -21,23 +21,33 @@ public class Main {
         /*..........................Второй запрос.........................................................*/
         User user = new User(3L,"James","Brown", (byte) 45);
         RestTemplate restTemplate2 = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Cookie",cookies);
-        //headers.add("Cookie", cookies);
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity requestEntity = new HttpEntity(user, headers);
-
-        ResponseEntity<String> responseEntity2 = restTemplate2.postForEntity(url,user, String.class);
-        //ResponseEntity response = restTemplate1.exchange(url, HttpMethod.POST, requestEntity, User.class);
+        HttpHeaders headers2 = new HttpHeaders();
+        headers2.add("Cookie",cookies);
+        headers2.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity requestEntity2 = new HttpEntity(user, headers2);
+        ResponseEntity<String> responseEntity2 = restTemplate2.exchange(url, HttpMethod.POST, requestEntity2, String.class);
         System.out.println(responseEntity2.getBody());
 
         /*..........................Третий запрос.........................................................*/
+        user.setName("Thomas");
+        user.setLastName("Shelby");
+        RestTemplate restTemplate3 = new RestTemplate();
+        HttpHeaders headers3 = new HttpHeaders();
+        headers3.add("Cookie",cookies);
+        headers3.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity requestEntity3 = new HttpEntity(user, headers3);
+        ResponseEntity<String> responseEntity3 = restTemplate3.exchange(url, HttpMethod.POST, requestEntity3, String.class);
+        System.out.println(responseEntity3.getBody());
 
+        /*..........................Четвертый запрос.........................................................*/
 
-
-
-
-
+        RestTemplate restTemplate4 = new RestTemplate();
+        HttpHeaders headers4 = new HttpHeaders();
+        headers4.add("Cookie",cookies);
+        headers4.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity requestEntity4 = new HttpEntity(user, headers4);
+        ResponseEntity<String> responseEntity4 = restTemplate4.exchange (url+"/"+2,HttpMethod.DELETE, requestEntity4, String.class);
+        System.out.println(responseEntity4.getBody());
 
 
 
